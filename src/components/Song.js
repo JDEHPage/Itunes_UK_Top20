6192303c3d29a94cfbe356de81ca6bc74ba028e7
John Page <jdehpage@gmail.com>
2019-04-10 10:01:09 +0100

    Song Layout updated

--- src/components/Song.js
+++ src/components/Song.js
@@ -5,12 +5,10 @@ const Song = (props) => {
   return(
 
     <div className="songs">
+      <h2>No.{ props.position+1 }</h2>
       <img src={ props.song["im:image"][2].label } alt="blank"/>
       <p>Title: { props.song["im:name"].label }</p>
       <p>Artist: { props.song["im:artist"].label }</p>
-
-      
-      <p>Rank: { props.position+1 }</p>
       <audio controls="controls" src={ props.song.link[1].attributes.href }/>
     </div>
   )
